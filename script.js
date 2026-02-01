77aa4b6272b11e69fabf4ee3482cab85c0a83733
rautp2226-jpg <rautp2226@gmail.com>
2026-02-01 17:11:17 +0530

    Add files via upload
    updated yes page, hearts animation, No button fix

--- script.js
+++ script.js
@@ -2,15 +2,33 @@ const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const message = document.getElementById("message");
 
-noBtn.addEventListener("mouseenter", () => {
-    const maxX = 200;
-    const maxY = 100;
-    const randomX = Math.floor(Math.random()*maxX);
-    const randomY = Math.floor(Math.random()*maxY);
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
+if (noBtn) {
+    noBtn.addEventListener("mouseenter", () => {
+        const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+        const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+        noBtn.style.left = x + "px";
+        noBtn.style.top = y + "px";
+    });
+}
+yesBtn.addEventListener("click", () => {
+    window.location.href = "yes.html";
 });
 
-yesBtn.addEventListener("click", () => {
-    message.innerHTML = "YAYYYY😁😁😁😘😘😘💕💕I KNEW ITT💕!LOVE YOU BABYY😘😘 ";
-});
+const heartsContainer = document.querySelector(".hearts");
+function createHeart() {
+    if (!heartsContainer) return;
+
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "💕";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = Math.random() * 20 + 15 + "px";
+    heartsContainer.appendChild(heart);
+    setTimeout(() => {
+        heart.remove();
+    },4000);
+}
+if (heartsContainer){
+    setInterval(createHeart,250);
+}
